e206b7a7d08f24dc2dd019682551f782dd9de73d
Vaibhav211960 <160199743+Vaibhav211960@users.noreply.github.com>
2026-05-04 19:58:31 +0530

    vercel fixed

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,15 +2,11 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import tailwindcss from "@tailwindcss/vite"
 import path from "path"
-// import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal"
-// import { metaImagesPlugin } from "./vite-plugin-meta-images.js"
 
 export default defineConfig(async () => {
   const plugins = [
     react(),
-    // runtimeErrorOverlay(),
     tailwindcss(),
-    // metaImagesPlugin(),
   ]
 
   if (
@@ -28,6 +24,8 @@ export default defineConfig(async () => {
   }
 
   return {
+    base: "/",   // ✅ CORRECT PLACE
+
     plugins,
 
     resolve: {
@@ -43,6 +41,5 @@ export default defineConfig(async () => {
         plugins: [],
       },
     },
-
   }
 })
